7d62916ea9651a1e6eeb48c81575bc184bb6f561
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-03-24 08:41:53 -0700

    add uart

---
+++ include/uart.h
@@ -0,0 +1,14 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+#include "gpio.h"
+
+
+
+void uart_init();
+void uart_send(unsigned int c);
+char uart_getc();
+void uart_puts(char *s);
+void uart_hex(unsigned int d);
+
+#endif
